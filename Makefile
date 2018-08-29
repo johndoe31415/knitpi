afdee59649049a1697f10f7a46543ad6ead2acb4
Johannes Bauer <joe@johannes-bauer.com>
2018-08-29 17:45:45 +0200

    Added simple GPIO thread function
    Have a callback execute asynchronously on any event.

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@
 
 CFLAGS := -O3 -Wall -std=c11 -D_POSIX_C_SOURCE=199309L -D_XOPEN_SOURCE=500 -Wstrict-prototypes
 LDFLAGS := -pthread -lgpiod
-OBJS := peripherals.o peripherals_gpio.o peripherals_spi.o test_gpio.o
+OBJS := peripherals.o peripherals_gpio.o peripherals_spi.o test_gpio.o gpio_thread.o
 BINARIES := test_gpio
 
 all: $(BINARIES)
 
-test_gpio: peripherals.o peripherals_gpio.o peripherals_spi.o test_gpio.o
+test_gpio: peripherals.o peripherals_gpio.o peripherals_spi.o test_gpio.o gpio_thread.o
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 clean:
---
+++ gpio_thread.c
@@ -0,0 +1,51 @@
+/*
+ *	knitpi - Raspberry Pi interface for Brother KH-930 knitting machine
+ *	Copyright (C) 2018-2018 Johannes Bauer
+ *
+ *	This file is part of knitpi.
+ *
+ *	knitpi is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; this program is ONLY licensed under
+ *	version 3 of the License, later versions are explicitly excluded.
+ *
+ *	knitpi is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with knitpi; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *	Johannes Bauer <JohannesBauer@gmx.de>
+ */
+
+#include <stdio.h>
+#include <pthread.h>
+#include "gpio_thread.h"
+#include "peripherals_gpio.h"
+
+static bool run_thread;
+
+static void* gpio_thread_body(void *vhandler) {
+	gpio_irq_callback_t handler = (gpio_irq_callback_t)vhandler;
+	while (run_thread) {
+		gpio_wait_for_input_change(handler, 5000);
+	}
+	return NULL;
+}
+
+void start_gpio_thread(gpio_irq_callback_t irq_handler) {
+	pthread_attr_t attrs;
+	pthread_attr_init(&attrs);
+	pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_DETACHED);
+
+	pthread_t thread;
+	run_thread = true;
+	pthread_create(&thread, &attrs, gpio_thread_body, irq_handler);
+}
+
+void stop_gpio_thread(void) {
+	run_thread = false;
+}
